e25a705f09623c4c624383f0502578648c51aeca
Jack Feser <feser@csail.mit.edu>
2020-01-17 12:00:25 -0500

    Flesh out interface with Of_list modules.

--- lib/combinat.ml
+++ lib/combinat.ml
@@ -32,8 +32,22 @@ module Unsafe = struct
   end
 end
 
-module Partition = Container.Make0 (struct
+module type PARTITION = sig
   type t = int * int
+
+  val create : n:int -> parts:int -> t
+
+  include Container.S0 with type t := t and type elt := int_array
+end
+
+module Partition_t = struct
+  type t = int * int
+
+  let create ~n ~parts = (n, parts)
+end
+
+module Partition_core = struct
+  include Partition_t
 
   module Elt = struct
     type t = int_array
@@ -89,7 +103,12 @@ module Partition = Container.Make0 (struct
   let iter = `Define_using_fold
 
   let length = `Define_using_fold
-end)
+end
+
+module Partition = struct
+  include Partition_t
+  include Container.Make0 (Partition_core)
+end
 
 let%expect_test "m_partition_iter" =
   Partition.iter
@@ -118,31 +137,34 @@ let%expect_test "m_partition_iter" =
     (3 2 1)
     (2 2 2) |}]
 
-module Partition_with_zeros = Container.Make0 (struct
-  type t = int * int
+let%expect_test "m_partition_iter" =
+  Partition.iter
+    ~f:(fun c ->
+      Stdio.print_endline (Sexp.to_string_hum ([%sexp_of: int array] (to_array c))))
+    (0, 2);
+  [%expect {| |}]
 
-  module Elt = struct
-    type t = int_array
+module Partition_with_zeros = struct
+  include Partition_t
 
-    let equal = Util.equal
-  end
+  include Container.Make0 (struct
+    include Partition_core
 
-  let fold (n, m) ~init ~f =
-    let arr = Bigarray.(Array1.create int c_layout m) in
-    let f acc c =
-      Array1.fill arr 0;
-      Array1.blit c (Array1.sub arr 0 (Array1.dim c));
-      f acc arr
-    in
-    let rec fold m' acc =
-      if m < m' then acc else fold (m' + 1) (Partition.fold (n, m') ~init:acc ~f)
-    in
-    fold 0 init
+    let orig_fold = fold
 
-  let iter = `Define_using_fold
-
-  let length = `Define_using_fold
-end)
+    let fold (n, m) ~init ~f =
+      let arr = Bigarray.(Array1.create int c_layout m) in
+      let f acc c =
+        Array1.fill arr 0;
+        Array1.blit c (Array1.sub arr 0 (Array1.dim c));
+        f acc arr
+      in
+      let rec fold m' acc =
+        if m < m' then acc else fold (m' + 1) (orig_fold (n, m') ~init:acc ~f)
+      in
+      fold 0 init
+  end)
+end
 
 let%expect_test "partition_with_zeros" =
   Partition_with_zeros.iter (6, 3) ~f:(fun c ->
@@ -157,9 +179,17 @@ let%expect_test "partition_with_zeros" =
     (3 2 1)
     (2 2 2) |}]
 
+module Permutation_t = struct
+  type t = int
+
+  let create x = x
+end
+
 module Permutation = struct
+  include Permutation_t
+
   include Container.Make0 (struct
-    type t = int
+    include Permutation_t
 
     module Elt = struct
       type t = int_array
@@ -210,22 +240,28 @@ module Permutation = struct
     let length = `Custom fact
   end)
 
-  module Of_list = Build.Make (struct
+  module Of_list = struct
     type 'a t = 'a list
 
-    type 'a elt = 'a list
+    let create = Fn.id
 
-    let fold l ~init ~f =
-      let n = List.length l in
-      let elems = List.to_array l in
-      fold n ~init ~f:(fun x a -> f x (List.init n ~f:(fun i -> elems.(a.{i}))))
+    include Build.Make (struct
+      type nonrec 'a t = 'a t
 
-    let iter = `Define_using_fold
+      type 'a elt = 'a list
 
-    let length =
-      let length l = length (List.length l) in
-      `Custom length
-  end)
+      let fold l ~init ~f =
+        let n = List.length l in
+        let elems = List.to_array l in
+        fold n ~init ~f:(fun x a -> f x (List.init n ~f:(fun i -> elems.(a.{i}))))
+
+      let iter = `Define_using_fold
+
+      let length =
+        let length l = length (List.length l) in
+        `Custom length
+    end)
+  end
 end
 
 let%expect_test "permutations_iter" =
@@ -258,53 +294,82 @@ let%expect_test "permutations_iter" =
     (4 3 1 2)
     (4 3 2 1) |}]
 
-module SortedPermutation = Container.Make0 (struct
+module SortedPermutation = struct
   type t = int * (int -> int -> bool)
 
-  module Elt = struct
-    type t = int_array * int_array
+  let create n compare = (n, compare)
 
-    let equal (a1, a2) (b1, b2) = Util.equal a1 b1 && Util.equal a2 b2
-  end
+  include Container.Make0 (struct
+    type nonrec t = t
 
-  let fold p ~init ~f =
-    let n, ( << ) = p in
-    let ( << ) x y =
-      match (x, y) with 0, 0 -> false | 0, _ -> true | x, y -> x << y
-    in
-    let a = Bigarray.(Array1.create int c_layout (n + 1)) in
-    let a' = Bigarray.(Array1.create int c_layout (n + 1)) in
-    let elem = (Bigarray.Array1.sub a 1 n, Bigarray.Array1.sub a' 1 n) in
-    for j = 0 to n do
-      a.{j} <- j;
-      a'.{j} <- j
-    done;
-    let rec v3 acc k =
-      let j = a'.{k} in
-      let l = a.{j - 1} in
-      if l << k then (
-        for j = j to k - 1 do
-          let l = a.{j + 1} in
+    module Elt = struct
+      type t = int_array * int_array
+
+      let equal (a1, a2) (b1, b2) = Util.equal a1 b1 && Util.equal a2 b2
+    end
+
+    let fold p ~init ~f =
+      let n, ( << ) = p in
+      let ( << ) x y =
+        match (x, y) with 0, 0 -> false | 0, _ -> true | x, y -> x << y
+      in
+      let a = Bigarray.(Array1.create int c_layout (n + 1)) in
+      let a' = Bigarray.(Array1.create int c_layout (n + 1)) in
+      let elem = (Bigarray.Array1.sub a 1 n, Bigarray.Array1.sub a' 1 n) in
+      for j = 0 to n do
+        a.{j} <- j;
+        a'.{j} <- j
+      done;
+      let rec v3 acc k =
+        let j = a'.{k} in
+        let l = a.{j - 1} in
+        if l << k then (
+          for j = j to k - 1 do
+            let l = a.{j + 1} in
+            a.{j} <- l;
+            a'.{l} <- j
+          done;
+          a.{k} <- k;
+          a'.{k} <- k;
+          let k = k - 1 in
+          if k > 0 then v3 acc k else acc )
+        else (
+          a.{j - 1} <- k;
           a.{j} <- l;
-          a'.{l} <- j
-        done;
-        a.{k} <- k;
-        a'.{k} <- k;
-        let k = k - 1 in
-        if k > 0 then v3 acc k else acc )
-      else (
-        a.{j - 1} <- k;
-        a.{j} <- l;
-        a'.{k} <- j - 1;
-        a'.{l} <- j;
-        v3 (f acc elem) n )
-    in
-    v3 (f init elem) n
+          a'.{k} <- j - 1;
+          a'.{l} <- j;
+          v3 (f acc elem) n )
+      in
+      v3 (f init elem) n
 
-  let iter = `Define_using_fold
+    let iter = `Define_using_fold
 
-  let length = `Define_using_fold
-end)
+    let length = `Define_using_fold
+  end)
+
+  module Of_list = struct
+    type 'a t = 'a list * ('a -> 'a -> bool)
+
+    include Build.Make (struct
+      type nonrec 'a t = 'a t
+
+      type 'a elt = 'a list
+
+      let fold (l, cmp) ~init ~f =
+        let n = List.length l in
+        let elems = List.to_array l in
+        let cmp i i' = cmp elems.(i) elems.(i') in
+        fold (create n cmp) ~init ~f:(fun x (a, _) ->
+            f x (List.init n ~f:(fun i -> elems.(a.{i}))))
+
+      let iter = `Define_using_fold
+
+      let length = `Define_using_fold
+    end)
+
+    let create l cmp = (l, cmp)
+  end
+end
 
 let%expect_test "sorted_permutations_iter" =
   SortedPermutation.iter
@@ -380,58 +445,86 @@ let%expect_test "sorted_permutations_young" =
     (1 4 6 2 5 8 3 7 9)
     (1 4 7 2 5 8 3 6 9) |}]
 
-module RestrictedPermutation = Container.Make0 (struct
+module RestrictedPermutation = struct
   type t = int * (int_array -> bool)
 
-  module Elt = struct
-    type t = int_array
+  include Container.Make0 (struct
+    type nonrec t = t
 
-    let equal = Util.equal
+    module Elt = struct
+      type t = int_array
+
+      let equal = Util.equal
+    end
+
+    let fold (n, t) ~init ~f =
+      let a = Bigarray.(Array1.create int c_layout (n + 1)) in
+      let l = Bigarray.(Array1.create int c_layout (n + 1)) in
+      let u = Bigarray.(Array1.create int c_layout (n + 1)) in
+      let t_args = Array.create ~len:(n + 1) (Bigarray.Array1.sub a 1 1) in
+      for k = 1 to n do
+        t_args.(k) <- Bigarray.Array1.sub a 1 k
+      done;
+      let elem = Bigarray.Array1.sub a 1 n in
+      for k = 0 to n - 1 do
+        l.{k} <- k + 1
+      done;
+      l.{n} <- 0;
+      let rec x3 acc k p q =
+        a.{k} <- q;
+        if t t_args.(k) then
+          if k = n then x6 (f acc elem) k
+          else (
+            u.{k} <- p;
+            l.{p} <- l.{q};
+            x3 acc (k + 1) 0 l.{0} )
+        else
+          let p = q in
+          let q = l.{p} in
+          if q = 0 then x6 acc k else x3 acc k p q
+      and x6 acc k =
+        let k = k - 1 in
+        if k = 0 then acc
+        else
+          let p = u.{k} in
+          let q = a.{k} in
+          l.{p} <- q;
+          let p = q in
+          let q = l.{p} in
+          if q = 0 then x6 acc k else x3 acc k p q
+      in
+      x3 init 1 0 l.{0}
+
+    let iter = `Define_using_fold
+
+    let length = `Define_using_fold
+  end)
+
+  let create n f = (n, f)
+
+  module Of_list = struct
+    type 'a t = 'a list * ('a list -> bool)
+
+    include Build.Make (struct
+      type nonrec 'a t = 'a t
+
+      type 'a elt = 'a list
+
+      let fold (l, filter) ~init ~f =
+        let n = List.length l in
+        let elems = List.to_array l in
+        let filter a = filter (List.init n ~f:(fun i -> elems.(a.{i}))) in
+        fold (create n filter) ~init ~f:(fun x a ->
+            f x (List.init n ~f:(fun i -> elems.(a.{i}))))
+
+      let iter = `Define_using_fold
+
+      let length = `Define_using_fold
+    end)
+
+    let create l f = (l, f)
   end
-
-  let fold p ~init ~f =
-    let n, t = p in
-    let a = Bigarray.(Array1.create int c_layout (n + 1)) in
-    let l = Bigarray.(Array1.create int c_layout (n + 1)) in
-    let u = Bigarray.(Array1.create int c_layout (n + 1)) in
-    let t_args = Array.create ~len:(n + 1) (Bigarray.Array1.sub a 1 1) in
-    for k = 1 to n do
-      t_args.(k) <- Bigarray.Array1.sub a 1 k
-    done;
-    let elem = Bigarray.Array1.sub a 1 n in
-    for k = 0 to n - 1 do
-      l.{k} <- k + 1
-    done;
-    l.{n} <- 0;
-    let rec x3 acc k p q =
-      a.{k} <- q;
-      if t t_args.(k) then
-        if k = n then x6 (f acc elem) k
-        else (
-          u.{k} <- p;
-          l.{p} <- l.{q};
-          x3 acc (k + 1) 0 l.{0} )
-      else
-        let p = q in
-        let q = l.{p} in
-        if q = 0 then x6 acc k else x3 acc k p q
-    and x6 acc k =
-      let k = k - 1 in
-      if k = 0 then acc
-      else
-        let p = u.{k} in
-        let q = a.{k} in
-        l.{p} <- q;
-        let p = q in
-        let q = l.{p} in
-        if q = 0 then x6 acc k else x3 acc k p q
-    in
-    x3 init 1 0 l.{0}
-
-  let iter = `Define_using_fold
-
-  let length = `Define_using_fold
-end)
+end
 
 let%expect_test "restricted_permutations_iter" =
   RestrictedPermutation.iter
--- lib/combinat.mli
+++ lib/combinat.mli
@@ -2,30 +2,70 @@ open Base
 
 type int_array = (int, Bigarray.int_elt, Bigarray.c_layout) Bigarray.Array1.t
 
+module type PARTITION = sig
+  type t = int * int
+
+  val create : n:int -> parts:int -> t
+
+  include Container.S0 with type t := t and type elt := int_array
+end
+
+module Partition : PARTITION
 (** Compute the partitions of an integer {i n} into {i m} parts. See
     (Knuth 3b, pg. 2). *)
-module Partition : Container.S0 with type t := int * int and type elt := int_array
 
+module Partition_with_zeros : PARTITION
 (** Compute the partitions of an integer {i n} into {i m} parts,
     including partitions where some elements are zero. *)
-module Partition_with_zeros :
-  Container.S0 with type t := int * int and type elt := int_array
 
 (** Compute the unique permutations of an array. See (Knuth 2b, pg. 1). *)
 module Permutation : sig
-  include Container.S0 with type t := int and type elt := int_array
+  type t = int
 
-  module Of_list :
-    Container.Generic with type 'a t := 'a list and type 'a elt := 'a list
+  val create : int -> t
+
+  include Container.S0 with type t := t and type elt := int_array
+
+  module Of_list : sig
+    type 'a t
+
+    val create : 'a list -> 'a t
+
+    include Container.Generic with type 'a t := 'a t and type 'a elt := 'a list
+  end
 end
 
-module SortedPermutation :
-  Container.S0
-    with type t := int * (int -> int -> bool)
-     and type elt := int_array * int_array
+module SortedPermutation : sig
+  type t
 
-module RestrictedPermutation :
-  Container.S0 with type t := int * (int_array -> bool) and type elt := int_array
+  val create : int -> (int -> int -> bool) -> t
+
+  include Container.S0 with type t := t and type elt := int_array * int_array
+
+  module Of_list : sig
+    type 'a t
+
+    val create : 'a list -> ('a -> 'a -> bool) -> 'a t
+
+    include Container.Generic with type 'a t := 'a t and type 'a elt := 'a list
+  end
+end
+
+module RestrictedPermutation : sig
+  type t
+
+  val create : int -> (int_array -> bool) -> t
+
+  include Container.S0 with type t := t and type elt := int_array
+
+  module Of_list : sig
+    type 'a t
+
+    val create : 'a list -> ('a list -> bool) -> 'a t
+
+    include Container.Generic with type 'a t := 'a t and type 'a elt := 'a list
+  end
+end
 
 (** Compute all of the {i t} combinations of the numbers in [0, {i n}]. *)
 module Combination : sig
